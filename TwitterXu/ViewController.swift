75d5f980624790c3f24bc00f543236a70b5dafa2
Nguyễn Thị Xuân Phương <xanhduong2000@gmail.com>
2016-10-29 09:33:41 +0700

    Initial Commit

---
+++ TwitterXu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterXu
+//
+//  Created by Phương Nguyễn on 10/29/16.
+//  Copyright © 2016 Phương Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
